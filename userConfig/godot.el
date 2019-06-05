b9e193e9dfe408bbc508e58a9e25ee55e6c542cc
SirRamESQ <gmrl95@gmail.com>
2019-06-05 14:58:20 -0400

    Better Godot Support

--- userConfig/godot.el
+++ userConfig/godot.el
@@ -1,3 +1,9 @@
+;; Location of Godot Package
+(add-to-list 'load-path "~/.emacs.d/packages/godot-gdscript.el/")
+
+;; load the package
+(load "godot-gdscript")
+
 (require 'toml-mode)
 
 ;; Register the desired extensions.
